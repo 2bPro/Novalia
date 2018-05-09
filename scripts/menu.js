c770c30e0dfd99216f6eda641d79a02c2d2a0b6f
2bPro <b.prodan95@gmail.com>
2018-05-09 07:55:51 +0100

    Novalia - a website for innovative Arduino tech

--- scripts/menu.js
+++
@@ -1,7 +0,0 @@
-$('.menu , .linee').on('click', function() {
-  $('.menu').toggleClass('over')
-  $('.linea1').toggleClass('overL1')
-  $('.linea2').toggleClass('overL2')
-  $('.linea3').toggleClass('overL3')
-  $('.menu-links').toggleClass('overmain')
-});
